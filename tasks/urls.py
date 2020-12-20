7ee942849d3a08d8814b3242948df7ac41ecdd0c
unknown <kumar.ankit383@gmail.com>
2020-12-21 00:15:59 +0530

    Upcoming, missed and complete task module API added

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.tasks, name='tasks')
+    path('', views.tasks, name='tasks'),
+    path('complete/<int:task_id>/', views.completeTask, name='complete_task'),
+    path('upcoming/', views.upcomingTasks, name='upcoming tasks'),
+    path('missed/', views.missedTasks, name='missedtask')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,6 @@
+from datetime import date, datetime
+
+from django.db.models.query_utils import Q
 from tasks.models import Task
 from django.http import JsonResponse, HttpResponse
 
@@ -9,6 +12,8 @@ from .serializers import TaskSerializer
 from rest_framework.response import Response
 from django.utils import timezone
 
+
+#   for current day tasks (today's tasks)
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 @authentication_classes([TokenAuthentication])
@@ -21,3 +26,58 @@ def tasks(request) :
         TaskSerializer(tasks, many=True).data,
         status=200
     )
+
+
+#   for upcoming tasks (future tasks)
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+@authentication_classes([TokenAuthentication])
+def upcomingTasks(request) : 
+    user = request.user
+    tasks = Task.objects.filter(
+        user = user,
+        completion_date__gt = datetime.now().date(),
+        status = False
+    )
+
+    return Response(
+        TaskSerializer(tasks, many=True).data,
+        status=200
+    )
+
+#   for tasks which have been completed and not deleted
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+@authentication_classes([TokenAuthentication])
+def missedTasks(request) : 
+    user = request.user
+    tasks = Task.objects.filter(
+        Q(user = user) & Q(status = False) & Q(completion_date__lt = datetime.now().date())
+    )
+
+    return Response(
+        TaskSerializer(tasks, many=True).data,
+        status=200
+    )
+
+#   for the tasks which has been missed (neither completed nor deleted)
+
+
+#   function to complete the status of the task
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+@authentication_classes([TokenAuthentication])
+def completeTask(request, task_id) : 
+    print("Task id = ", task_id)
+    try : 
+        task = Task.objects.get(id = task_id)
+        task.status = True
+        task.save()
+        return Response({
+            'message' : 'Saved successfully'
+        })
+    except Task.DoesNotExist :
+        return Response({
+            'error' : 'Task was not not found.'
+        }, status=status.HTTP_404_NOT_FOUND)
+
